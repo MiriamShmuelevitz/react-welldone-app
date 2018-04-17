14dcb400b048ff1cce39f206d3a7dedb5f97a578
Miriam Shmuelevitz <miriams@shefing.com>
2018-04-17 22:23:54 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      
+      <div>
+        aaa
+      </div>
+    );
+  }
+}
+
+export default App;
